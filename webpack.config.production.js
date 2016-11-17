589581ac129211607c1a123be163f31524780888
Ties de Kock <ties@tiesdekock.nl>
2016-11-17 10:10:34 +0100

    Create external sourcemap

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -5,27 +5,28 @@ const config = require('./webpack.config');
 
 
 module.exports = {
-    entry: config.entry,
-    module: config.module,
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-          minimize: true,
-          debug: false
-        }),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.EnvironmentPlugin([
-            'NODE_ENV',
-        ]),
-        new BabiliPlugin(),
-        new webpack.NoErrorsPlugin(),
-    ],
-    output: {
-        path: './dist/',
-        filename: 'duo-onderwijsdata.min.js',
-        libraryTarget: 'var',
-        library: 'DUOOnderwijsdata',
-    },
-    externals: config.externals,
-    resolve: config.resolve,
+  entry: config.entry,
+  module: config.module,
+  devtool: 'source-map',
+  plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.EnvironmentPlugin([
+          'NODE_ENV',
+      ]),
+      new BabiliPlugin(),
+      new webpack.NoErrorsPlugin(),
+  ],
+  output: {
+      path: './dist/',
+      filename: 'duo-onderwijsdata.min.js',
+      libraryTarget: 'var',
+      library: 'DUOOnderwijsdata',
+  },
+  externals: config.externals,
+  resolve: config.resolve,
 };
